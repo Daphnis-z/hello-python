c95fe4bd60cb66d7dce4e287c1a8920f6d23eeb4
Daphnis-z <daphnisz@163.com>
2020-06-02 21:40:27 +0800

    init program dir

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # hello-python
 It is a python project template,contains config,log,linux run script so on..
+
+
+
+# 前言
+
+准备做一个 Python 的项目模板，包含 **配置文件**、**程序日志**、**Linux 运行脚本** 等，后面把项目上值得固化的东西也放上来。
+
+因为现在项目一般都是基于 Python3 的，所以这个模板暂时也不考虑兼容 Python2。
+
+
+
+# 目录介绍
+
+按 github 上的顺序从上往下介绍顶级目录，次级目录就不介绍了。
+
+
+
+# 涵盖内容
+
+## 1. 配置文件
+
+## 2. 日志系统
+
+## 3. Linux 运行脚本
---
+++ bin/run-hello-python.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# -*- sh -*-
+
+# 获取程序的根目录，便于对资源进行定位
+PRG="$0"
+while [ -h "$PRG" ]; do
+        ls=`ls -ld "$PRG"`
+        link=`expr "$ls" : '.*-> \(.*\)$'`
+        if expr "$link" : '.*/.*' > /dev/null; then
+                PRG="$link"
+        else
+                PRG=`dirname "$PRG"`/"$link"
+        fi
+        done
+PRGDIR=`dirname "$PRG"`
+cd $PRGDIR/..
+
+# 获取程序根目录的绝对路径
+root_dir=`pwd`
+
+echo "program root dir: $root_dir"
